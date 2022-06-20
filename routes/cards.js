ce031e5363f4271a2f87f31c97dc39ad6decabbd
Stas Makarov <makarov.s.d@yandex.ru>
2022-06-20 16:51:50 +0300

    fix styles

--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,8 @@
 const cardsRouter = require('express').Router();
 const { getCards, createCard, deleteCard } = require('../controllers/card');
 
-cardsRouter.get('/cards', getCards);
-cardsRouter.post('/cards', createCard);
-cardsRouter.delete('/cards/:cardId', deleteCard);
+cardsRouter.get('/', getCards);
+cardsRouter.post('/', createCard);
+cardsRouter.delete('/:cardId', deleteCard);
 
 module.exports = cardsRouter;
