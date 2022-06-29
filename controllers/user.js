97aa78e8e7012b8405086128e78c19d37582f45b
Stas Makarov <makarov.s.d@yandex.ru>
2022-06-29 17:17:05 +0300

    fix styles

--- controllers/user.js
+++ controllers/user.js
@@ -42,14 +42,14 @@ module.exports.createUser = (req, res) => {
         email: user.email,
       },
     }))
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        res.status(400).send({
-          message: 'Переданы некорректные данные при создании пользователя',
-        });
-      } else {
-        res.status(500).send({ message: 'Ошибка по умолчанию' });
-      }
+    .catch(() => {
+      // if (err.name === 'ValidationError') {
+      //   res.status(400).send({
+      //     message: 'Переданы некорректные данные при создании пользователя',
+      //   });
+      // } else {
+      res.status(500).send({ message: 'Ошибка по умолчанию' });
+      // }
     });
 };
 
