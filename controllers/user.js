eedc9d76ee18e7f4aac934e250fd240d56dda9ea
Stas Makarov <makarov.s.d@yandex.ru>
2022-06-29 16:43:50 +0300

    fix styles

--- controllers/user.js
+++ controllers/user.js
@@ -34,10 +34,17 @@ module.exports.createUser = (req, res) => {
     .then((hash) => User.create({
       name, about, avatar, email, password: hash,
     }))
-    .then((user) => res.status(201).send({ data: user }))
+    .then((user) => res.status(201).send({
+      message: {
+        name: user.name,
+        about: user.about,
+        avatar: user.avatar,
+        email: user.email,
+      },
+    }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(409).send({
+        res.status(400).send({
           message: 'Переданы некорректные данные при создании пользователя',
         });
       } else {
