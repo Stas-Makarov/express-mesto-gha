c3aab0044cbc0fd5e5269ca9da1e9a1c7e17933a
Stas Makarov <makarov.s.d@yandex.ru>
2022-06-29 14:27:56 +0300

    fix styles

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().regex.pattern(/^https?:\/\/(www\.)?[a-zA-Z\d-]+\.[\w\d\-.~:/?#[\]@!$&'()*+,;=]{2,}#?$/),
+    avatar: Joi.string().regex(/^https?:\/\/(www\.)?[a-zA-Z\d-]+\.[\w\d\-.~:/?#[\]@!$&'()*+,;=]{2,}#?$/),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(4),
   }),
