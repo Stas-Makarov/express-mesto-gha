f3f69853d4f271dd93a3eec73319f4ddaf5173f4
Stas Makarov <makarov.s.d@yandex.ru>
2022-07-01 12:33:53 +0300

    fix styles

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,22 @@ const usersRouter = require('express').Router();
 const {
   getUsers, getUsersById, updateProfile, updateAvatar, getCurrentUser,
 } = require('../controllers/user');
+const { celebrate, Joi, errors } = require('celebrate');
+const { isURL } = require('validator');
 
 usersRouter.get('/', getUsers);
 usersRouter.get('/me', getCurrentUser);
 usersRouter.get('/:userId', getUsersById);
-usersRouter.patch('/me', updateProfile);
-usersRouter.patch('/me/avatar', updateAvatar);
+usersRouter.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), updateProfile);
+usersRouter.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().[isURL, 'Ошибка валидации'],
+  }),
+}), updateAvatar);
 
 module.exports = usersRouter;
