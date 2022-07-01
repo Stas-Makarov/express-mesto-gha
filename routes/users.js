3f4521b69b66fd6d77a0e7b34955090449dd8e5f
Stas Makarov <makarov.s.d@yandex.ru>
2022-07-01 11:58:56 +0300

    fix styles

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,11 @@
 const usersRouter = require('express').Router();
 const {
-  getUsers, createUser, getUsersById, updateProfile, updateAvatar, getCurrentUser,
+  getUsers, getUsersById, updateProfile, updateAvatar, getCurrentUser,
 } = require('../controllers/user');
 
 usersRouter.get('/', getUsers);
 usersRouter.get('/me', getCurrentUser);
 usersRouter.get('/:userId', getUsersById);
-usersRouter.post('/', createUser);
 usersRouter.patch('/me', updateProfile);
 usersRouter.patch('/me/avatar', updateAvatar);
 
