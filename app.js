bfc10b1bccc4d0d88b16c94ddb2284ee2fc08dad
Stas Makarov <makarov.s.d@yandex.ru>
2022-07-03 15:33:06 +0300

    fix review remarks

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const { errors, celebrate, Joi } = require('celebrate');
 const { login, createUser } = require('./controllers/user');
 const { auth } = require('./middlewares/auth');
 const regex = require('./utils/constans');
-const emailRegex = require('./utils/constans');
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -19,7 +18,7 @@ app.use(cookieParser());
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().pattern(emailRegex),
+    email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
 }), login);
@@ -28,7 +27,7 @@ app.post('/signup', celebrate({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().pattern(regex),
-    email: Joi.string().required().pattern(emailRegex),
+    email: Joi.string().required().email(),
     password: Joi.string().required().min(4),
   }),
 }), createUser);
--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const validator = require('validator');
 const regex = require('../utils/constans');
 const AuthError = require('../errors/AuthError');
 
@@ -33,6 +34,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: 'Недопустимый формат адреса почты',
+    },
   },
   password: {
     type: String,
