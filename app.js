10cdbfaf7bf4188a5dde10c449b735b0920beceb
Stas Makarov <makarov.s.d@yandex.ru>
2022-07-16 08:35:39 +0300

    add cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors, celebrate, Joi } = require('celebrate');
+const cors = require('cors');
 const { login, createUser } = require('./controllers/user');
 const { auth } = require('./middlewares/auth');
 const regex = require('./utils/constans');
@@ -15,8 +16,22 @@ const { PORT = 3000 } = process.env;
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(cors({
+  credentials: true,
+  origin: [
+    'https://localhost:3001',
+    'https://localhost:3000',
+    'http://localhost:3001',
+    'http://localhost:3000',
+    'http://api.s.d.domainname.students.nomoredomains.xyz',
+    'https://api.s.d.domainname.students.nomoredomains.xyz',
+    'http://s.d.domainname.students.nomoredomains.xyz',
+    'https://s.d.domainname.students.nomoredomains.xyz',
+  ],
+}));
 
 app.get('/crash-test', () => {
   setTimeout(() => {
